14b63b9cdcb89b86864b15c49cb125754e73ca30
Albatrosovs <71939238+Albatrosovs@users.noreply.github.com>
2022-01-19 23:02:54 +0300

    Delete Balloon directory

--- Balloon/constants.js
+++
@@ -1,9 +0,0 @@
-export const PARTICLE_RADIUS = 2.5;
-export const PARTICLE_COLOR = "#0000FF";
-export const DEFAULT_COLOR = "#000000";
-export const SPRING_COLOR = DEFAULT_COLOR;
-export const WALL_COLOR = "#711F29";
-export const WIDTH = 700;
-export const HEIGHT = 700;
-export const EPSILON = 1;
-export const SIGMA = 1;
